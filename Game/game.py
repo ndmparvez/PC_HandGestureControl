1a82c73be81402f6d43b0c174868401c6f68bbab
ndmparvez <71454390+ndmparvez@users.noreply.github.com>
2021-09-22 22:59:47 +0530

    Add files via upload

---
+++ Game/game.py
@@ -0,0 +1,30 @@
+import serial
+import time
+import pyautogui
+
+ser= serial.Serial('COM3',9600)
+time.sleep(3)
+
+pyautogui.hotkey('alt','win','r')
+while 1:
+    val =str( ser.readline() )
+    
+
+    if 'Up' in val :
+        pyautogui.typewrite(['up'])
+        print(val)
+    
+    if 'Down' in val :
+        pyautogui.typewrite(['down'])
+        print(val)
+        
+    if 'Right' in val :
+        pyautogui.typewrite(['right'])
+        print(val)
+
+        
+    if 'Left' in val:
+        pyautogui.typewrite(['left'])
+        print(val)
+
+                     
